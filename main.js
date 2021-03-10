263914c1ecd6b47702447047b17e1603696b4090
otkmaster <33026818+otkmaster@users.noreply.github.com>
2021-03-10 14:49:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -707,7 +707,7 @@ function main() {
         dailyQuestion(); //每日答题
         }   
       if (myScores['每周答题'] != 5) {
-        if （time.getDay() = 3）{
+        if （time.getDay() == 3）{
         weeklyQuestion(); //每周答题 }
         }        
       if (myScores['专项答题'] != 10) {
